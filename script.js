01571d4195715052cee0e46e209a9bd205f6e7ed
MakarukArsen <makarukarsen@gmail.com>
2022-02-19 18:12:42 +0200

    1.0

---
+++ script.js
@@ -0,0 +1,26 @@
+let n = +prompt("Введіть перше число", 1);
+while(Number.isNaN(n) || !Number.isInteger(n) || n == String() || n == null){
+    alert("Це не число");
+    n = +prompt("Введіть перше число", 1);
+}
+
+let m = +prompt("Введіть друге число", 10);
+while(Number.isNaN(m) || !Number.isInteger(m) || m == String() || m == null){
+    alert("Це не число");
+    m = +prompt("Введіть друге число", 10);
+}
+
+let skipPairNumbers = confirm("Пропускати парні числа?");
+for(let total = 0; n <= m; ++n){
+    if(skipPairNumbers){
+        if((n % 2) === 0){
+            continue
+        }
+        total += n
+        console.log(total)
+    }
+    if(!skipPairNumbers){
+        total += n
+        console.log(total)
+    }
+}
