8a1094e7edf4bee87018d9bdebb9cc3a22ed1d3e
MakarukArsen <makarukarsen@gmail.com>
2022-02-22 19:21:56 +0200

    3.0

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let n = +prompt("Введіть перше число", 1);
-while(Number.isNaN(n) || !Number.isInteger(n) || n == String() || n == null){
+while(Number.isNaN(n) || !Number.isInteger(n) || n == null){
     alert("Це не число");
     n = +prompt("Введіть перше число", 1);
 }
 
 let m = +prompt("Введіть друге число", 10);
-while(Number.isNaN(m) || !Number.isInteger(m) || m == String() || m == null){
+while(Number.isNaN(m) || !Number.isInteger(m) || m == null){
     alert("Це не число");
     m = +prompt("Введіть друге число", 10);
 }
@@ -13,16 +13,14 @@ while(Number.isNaN(m) || !Number.isInteger(m) || m == String() || m == null){
 let skipPairNumbers = confirm("Пропускати парні числа?");
 let total;
 for(total = 0; n <= m; ++n){
-    if(skipPairNumbers){
-        if((n % 2) === 0){
-            continue
-        }
+    if(skipPairNumbers && n % 2 == 0 ){
+        continue
         total += n
         console.log(total)
-    }
-    if(!skipPairNumbers){
+    } else{
         total += n
         console.log(total)
+        
     }
 }
 alert(total);
